552687ba1feb843603230bf7cb5d6fe9e3254af9
michael <michael@sofdigital.net>
2020-11-25 17:29:06 -0600

    Add project files.

---
+++ BenfordsElection/App.xaml.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows;
+
+namespace BenfordsElection
+{
+    public partial class App : Application
+    {
+    }
+}
---
+++ BenfordsElection/ViewModels/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using OxyPlot;
+using OxyPlot.Series;
+using System;
+
+namespace BenfordsElection.ViewModels
+{
+    public class MainViewModel
+    {
+        public PlotModel MyModel { get; private set; }
+        public MainViewModel()
+        {
+            this.MyModel = new PlotModel { Title = "Example 1" };
+            this.MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+        }
+    }
+}
